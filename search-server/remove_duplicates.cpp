830c0634f9d466ded13d0ef9f0fcd25206fd87c8
ElenaKad <119409473+ElenaKad@users.noreply.github.com>
2023-03-06 20:50:10 +0300

    sprint 6 final task 6

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,25 +1,27 @@
 #include "remove_duplicates.h"
 
+using namespace std::string_literals;
+
 void RemoveDuplicates(SearchServer& search_server) {
-	std::map<std::vector<std::string>, int> doc_words;
-	std::vector<int> duplicate_ids;
+    std::set<int> dublicates_docs;
 
-	for (const int document_id : search_server) {
-		auto word_freqs = search_server.GetWordFrequencies(document_id);
-		std::vector<std::string> words(word_freqs.size());
-		std::transform(word_freqs.cbegin(), word_freqs.cend(),
-			std::inserter(words, words.begin()), [](const std::pair<std::string, double>& elements) {
-				return elements.first;
-			});
+    std::set<std::set<std::string>> cash;
+    for (auto it = search_server.begin(); it != search_server.end(); ++it) {
+        const std::map<std::string, double> doc_words = search_server.GetWordFrequencies(*it);
+        std::set<std::string> words;
+        for (const auto& [key, _] : doc_words) {
+            words.insert(key);
+        }
+        if (cash.count(words)) {
+            dublicates_docs.insert(*it);
+        }
+        else {
+            cash.insert(words);
+        }
+    }
 
-		auto [word, emplaced] = doc_words.emplace(words, document_id);
-		if (!emplaced) {
-			duplicate_ids.push_back(document_id);
-		}
-	}
-
-	for (const int document_id : duplicate_ids) {
-		std::cout << "Found duplicate document id "s << document_id << std::endl;
-		search_server.RemoveDocument(document_id);
-	}
+    for (int i : dublicates_docs) {
+        std::cout << "Found duplicate document id "s << i << std::endl;
+        search_server.RemoveDocument(i);
+    }
 }
